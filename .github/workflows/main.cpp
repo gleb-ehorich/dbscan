93c307bf0c4fa8f5cbd6e662fea5a0c5f4937154
Gleb Rudenko <63010733+gleb-ehorich@users.noreply.github.com>
2020-07-01 12:47:16 +0300

    Update main.cpp

--- .github/workflows/main.cpp
+++ .github/workflows/main.cpp
@@ -1,22 +1,17 @@
 #include <stdio.h>
 #include <iostream>
 #include "dbscan.h"
-
 #define MINIMUM_POINTS 4     // minimum number of cluster
 #define EPSILON (0.75*0.75)  // distance for clustering, metre^2
-
 void readBenchmarkData(vector<Point>& points)
 {
     // load point cloud
     FILE *stream;
     stream = fopen ("benchmark_hepta.dat","ra");
-
     unsigned int minpts, num_points, cluster, i = 0;
     double epsilon;
     fscanf(stream, "%u\n", &num_points);
-
     Point *p = (Point *)calloc(num_points, sizeof(Point));
-
     while (i < num_points)
     {
           fscanf(stream, "%f,%f,%f,%d\n", &(p[i].x), &(p[i].y), &(p[i].z), &cluster);
@@ -24,11 +19,9 @@ void readBenchmarkData(vector<Point>& points)
           points.push_back(p[i]);
           ++i;
     }
-
     free(p);
     fclose(stream);
 }
-
 void printResults(vector<Point>& points, int num_points)
 {
     int i = 0;
@@ -45,22 +38,16 @@ void printResults(vector<Point>& points, int num_points)
           ++i;
     }
 }
-
 int main()
 {    
     vector<Point> points;
-
     // read point data
     readBenchmarkData(points);
-
     // constructor
     DBSCAN ds(MINIMUM_POINTS, EPSILON, points);
-
     // main loop
     ds.run();
-
     // result of DBSCAN algorithm
     printResults(ds.m_points, ds.getTotalPointSize());    
-
     return 0;
 }
